58b9ea815c4a77abfb71d90ae95410dfb95a1104
kamcoi <aimanadamx@gmail.com>
2021-02-08 19:30:59 +0800

    fix

--- wad/server.php
+++ wad/server.php
@@ -57,7 +57,7 @@ if (isset($_POST['reg_user'])) {
 
   // Finally, register user if there are no errors in the form
   if (count($errors) == 0) {
-  	$password = md5($password_1);//encrypt the password before saving in the database
+  	$password = $password_1;
 
   	$query = "INSERT INTO users (username, email, password) 
   			  VALUES('$username', '$email', '$password')";
@@ -81,7 +81,6 @@ if (isset($_POST['login_user'])) {
     }
   
     if (count($errors) == 0) {
-        $password = md5($password);
         $query = "SELECT * FROM users WHERE username='$username' AND password='$password'";
         $results = mysqli_query($db, $query);
         if (mysqli_num_rows($results) == 1) {
@@ -107,7 +106,6 @@ if (isset($_POST['login_admin'])) {
   }
 
   if (count($errors) == 0) {
-      $password = md5($password);
       $query = "SELECT * FROM admin WHERE username='$username' AND password='$password'";
       $results = mysqli_query($db, $query);
       if (mysqli_num_rows($results) == 1) {
